cd5aa19d349c44f5a296e0bcc783556023807bd1
Justin Glazer <jglazer365@gmail.com>
2025-07-09 22:36:51 -0400

    added springboot initializer

---
+++ Open-Hearts-Springboot/src/main/java/open_hearts_springboot/OpenHeartsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package open_hearts_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenHeartsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenHeartsSpringbootApplication.class, args);
+	}
+
+}
---
+++ Open-Hearts-Springboot/src/test/java/open_hearts_springboot/OpenHeartsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package open_hearts_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenHeartsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
