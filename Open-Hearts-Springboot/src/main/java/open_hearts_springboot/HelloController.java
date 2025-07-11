3be70f836af95be5a641cc189fbfbb6e3548acd6
Justin Glazer <jglazer365@gmail.com>
2025-07-10 21:33:45 -0400

    attempt one to add permissions through CORS

--- Open-Hearts-Springboot/src/main/java/open_hearts_springboot/HelloController.java
+++ Open-Hearts-Springboot/src/main/java/open_hearts_springboot/HelloController.java
@@ -1,5 +1,6 @@
 package open_hearts_springboot;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,9 @@ import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api")
-public class HelloController {
+@CrossOrigin(origins = "http://localhost:3000")
 
+public class HelloController {
 	@GetMapping("/")
 	public ResponseEntity<String> fetchData() {
 		return ResponseEntity.ok("Greetings from Spring Boot!");
