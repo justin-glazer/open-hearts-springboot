62b73a8d55d73d0b309c905859ee1264337bbfda
Justin Glazer <jglazer365@gmail.com>
2025-07-12 10:34:10 -0400

    finalized CORS configurations and updated API endpoint

---
+++ Open-Hearts-Springboot/src/main/java/open_hearts_springboot/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package open_hearts_springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Apply to all endpoints
+                        .allowedOrigins("http://localhost:3000") // Your frontend origin
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // if you're using cookies or auth headers
+            }
+        };
+    }
+}
